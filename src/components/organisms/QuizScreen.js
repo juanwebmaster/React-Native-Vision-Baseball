ed6429b8391159e9573ede7df34ad404926b82b1
Catalina <catalina@Catalinas-Mac.local>
2020-06-08 12:28:05 -0700

    Feat: ProgressCircle Added

--- src/components/organisms/QuizScreen.js
+++ src/components/organisms/QuizScreen.js
@@ -2,75 +2,25 @@ import React, {useState, useEffect, useRef} from 'react';
 import {get_quiz_data} from '../../apis';
 import SafeAreaView from 'react-native-safe-area-view';
 import {Text, StyleSheet, View, Image, TouchableOpacity} from 'react-native';
+import PrefersHomeIndicatorAutoHidden from 'react-native-home-indicator';
 import { Button } from 'react-native-elements';
 import Spinner from 'react-native-loading-spinner-overlay';
 import Orientation from 'react-native-orientation';
-import JWPlayer from 'react-native-jw-media-player';
-import ProgressBar from 'react-native-progress/Bar';
-// import {StyleSheet, ScrollView} from 'react-native';
-// import {Header} from 'react-native-elements';
-// import HeaderIcon from '_atoms/HeaderIcon';
-// import CustomFooter from '_organisms/CustomFooter';
-const BASE_IMAGE_URL = 'https://content.jwplatform.com/v2/media/';
-const BASE_VIDEO_URL = 'https://content.jwplatform.com/manifests/'
-
-
+import QuizView from '_components/organisms/QuizView'
 
 const QuizScreen = ({route, navigation}) => {
   const {data} = route.params;
   const [isLoading, setIsLoading] = useState(false);
   const [started, setStarted] = useState(false);
   const [currentOrientation, setCurrentOrientation] = useState(Orientation.getInitialOrientation());
-  const [index, setIndex] = useState(0);
-  const [imageUrl, setImageUrl] = useState(BASE_IMAGE_URL + data.question_ids[index].url + '/poster.jpg');
-  const [videoUrl, setVideoUrl] = useState(BASE_VIDEO_URL + data.question_ids[index].url + '.m3u8');
-  const [answers, setAnswers] = useState(data.question_ids[index].answers);
-  const [isPlaying, setIsPlaying] = useState('false');
-  const [progress, setProgress] = useState(0);
-  const progressRef = useRef(progress);
-  progressRef.current = progress;
-  //   const [levelData, setLevelData] = useState({});
-  const player = useRef(null);
-
-  
-  
   const handleOrientation = (orientation) => {
     setCurrentOrientation(orientation);
   };
   
-  const playlistItem= {
-    title: '',
-    mediaId: '1',
-    image: imageUrl,
-    desc: '',
-    time: 1,
-    file: videoUrl,
-//    file: 'http://file.com/file.mp3',
-    autostart: false,
-    controls: true,
-    repeat: false,
-    displayDescription: true,
-    displayTitle: true
-  }
+  useEffect(() => {
+    Orientation.addOrientationListener(handleOrientation);
+  }, []);
 
-  const onIdle = () => {
-    setIsPlaying(false);
-    const timerId = setInterval(() => {setProgress(progressRef.current + 0.1); console.log(progressRef.current)}, 200);
-    // after 5 seconds stop
-    
-    setTimeout(() => { clearInterval(timerId);  }, 3000);
-    setIndex(index + 1);
-    if (data.question_ids[index]) {
-      setImageUrl(BASE_IMAGE_URL + data.question_ids[index].url + '/poster.jpg');
-      setVideoUrl(BASE_VIDEO_URL + data.question_ids[index].url + '.m3u8');
-      setAnswers(data.question_ids[index].answers);
-    }
-  }
-  const onPlay = () => {
-    setIsPlaying(true);
-    setProgress(0);
-  }
-  
   const handleClick = () => {
     setStarted(true);
   }
@@ -93,94 +43,14 @@ const QuizScreen = ({route, navigation}) => {
       </View>
     );
   };
-  const QuizView = () => {
-    return (
-      <View style={{flex:1, flexDirection:'row'}}>
-        <View style={{ flex: 1, width:'50%' }}>
-          <JWPlayer
-            ref={player}
-            style={styles.player}
-            playlistItem={playlistItem} // Recommended - pass the playlistItem as a prop into the player
-            // playlist={[playlistItem]}
-            //onBeforePlay={() => this.onBeforePlay()}
-            onPlay={onPlay}
-            //onPause={() => this.onPause()}
-            onIdle={onIdle}
-            //onPlaylistItem={event => this.onPlaylistItem(event)}
-            //onSetupPlayerError={event => this.onPlayerError(event)}
-            //onPlayerError={event => this.onPlayerError(event)}
-            //onBuffer={() => this.onBuffer()}
-            //onTime={event => this.onTime(event)}
-            //onFullScreen={() => this.onFullScreen()}
-            //onFullScreenExit={() => this.onFullScreenExit()}
-          />
-        </View>
-        <View style={{ flex: 1, width:'50%', backgroundColor: '#333333',}}>
-          {!isPlaying && <AnswerView /> }
-        </View>
-      </View>
-      
-    )
-  }
 
-  const YesButton = ({title}) => {
-    return (
-      <TouchableOpacity style={{backgroundColor:"#7ed957", flex:1, margin:10, padding:10, justifyContent:'center', alignItems:'center', borderRadius:8}} >
-      <Text style={{color:'#ffffff',fontSize:17, paddingLeft:20, paddingRight:20, fontWeight:'900'}}>{title}</Text>
-      </TouchableOpacity>
-    )
-  }
-
-  const NoButton = ({title}) => {
-    return (
-      
-      <TouchableOpacity  style={{backgroundColor:"#ff5757", flex:1, margin:10, padding:10, justifyContent:'center', alignItems:'center', borderRadius:8}} >
-      <Text style={{color:'#ffffff',fontSize:17, paddingLeft:20, paddingRight:20, fontWeight:'900'}}>{title}</Text>
-      </TouchableOpacity>
-    )
-  }
-  const Answer = ({answer}) => {
-    return (
-      <View style={{alignItems:'center', }}>
-        <Text style={{color:'#ffffff', fontWeight:'400', fontSize:20}}>{answer.label}</Text>
-        <View style={{ flexDirection:'row'}}>
-          <YesButton title={answer.answer1} />
-          <NoButton title={answer.answer2} />
-        </View>
-      </View>
-    )
-    
-  }
-  const AnswerView = () => {
-    return (
-      <View style={{marginRight:50, flex:1, alignItems:'center'}}>
-        <View style={{flexDirection:'row', alignItems:'center', width:400, justifyContent:'space-between', paddingLeft:20}}>
-          <View>
-            <Text style={{color:'#ffffff',fontSize:20}}>{index + '/' + data.question_ids.length}</Text>
-            <Text style={{color:'#ffffff',fontSize:17}}>Pitch Count</Text>
-          </View>
-          
-          <TouchableOpacity  style={{backgroundColor:'#1E73BE', margin:10, padding:10, justifyContent:'center', alignItems:'center', borderRadius:8}} >
-            <Text style={{color:'#ffffff',fontSize:17, paddingLeft:20, paddingRight:20, fontWeight:'900'}}>Next</Text>
-          </TouchableOpacity>
-        </View>
-        {answers.map(item => <Answer answer={item} />)}
-        <Text style={{color:'#ffffff', fontWeight:'700',fontSize:17}}>TIMER</Text>
-        <ProgressBar progress={progress} width={400} height={20}/>
-      </View>
-    )
-  }
-
-  useEffect(() => {
-    Orientation.addOrientationListener(handleOrientation);
-    // return () => {
-    //   Orientation.removeOrientationListener(handleOrientation);
-    // };
-  }, []);
+  
   return (
     <View style={styles.container}>
+      <PrefersHomeIndicatorAutoHidden />
+      
       {currentOrientation === 'PORTRAIT' && <PortraitView />}
-      {currentOrientation === 'LANDSCAPE' && started && <QuizView />}
+      {currentOrientation === 'LANDSCAPE' && started && <QuizView data={data}/>}
       {currentOrientation === 'LANDSCAPE' && !started && <LandScapeView />}
       <Spinner
         visible={isLoading}
@@ -223,6 +93,8 @@ const styles = StyleSheet.create({
   player: {
     flex: 1,
     width: '100%',
+    justifyContent:'center',
+    alignItems:'center',
   }
 });
 export default QuizScreen;
---
+++ src/components/organisms/QuizView.js
@@ -0,0 +1,287 @@
+import React, {useState, useEffect, useRef} from 'react';
+import CountdownCircle from 'react-native-countdown-circle';
+import {
+  Text,
+  StyleSheet,
+  View,
+  Image,
+  TouchableOpacity,
+  Alert,
+} from 'react-native';
+import JWPlayer from 'react-native-jw-media-player';
+
+import {Overlay} from 'react-native-elements';
+
+const BASE_IMAGE_URL = 'https://content.jwplatform.com/v2/media/';
+const BASE_VIDEO_URL = 'https://content.jwplatform.com/manifests/';
+let questionId = 0;
+const QuizView = ({data}) => {
+  //const [questionId, setQuestionId] = useState(0);
+  console.disableYellowBox = true;
+  const [imageUrl, setImageUrl] = useState(
+    BASE_IMAGE_URL + data.question_ids[questionId].url + '/poster.jpg',
+  );
+  const [videoUrl, setVideoUrl] = useState(
+    BASE_VIDEO_URL + data.question_ids[questionId].url + '.m3u8',
+  );
+  const [answers, setAnswers] = useState(data.question_ids[questionId].answers);
+  const [isPlaying, setIsPlaying] = useState('');
+  const [selAnswer, setSelAnswer] = useState('');
+
+  const player = useRef(null);
+  const [visible, setVisible] = useState(true);
+  const handlePressVideo = () => {
+    // console.log(player.current.play());
+  };
+  const playlistItem = {
+    title: '',
+    mediaId: '1',
+    image: imageUrl,
+    desc: '',
+    time: 1,
+    file: videoUrl,
+    //    file: 'http://file.com/file.mp3',
+    autostart: false,
+    controls: false,
+    repeat: false,
+    displayDescription: true,
+    displayTitle: true,
+  };
+
+  const playList = data.question_ids.map((item, id) => {
+    return {
+      title: '',
+      mediaId: id,
+      image: imageUrl,
+      desc: '',
+      time: 1,
+      file: videoUrl,
+      //    file: 'http://file.com/file.mp3',
+      autostart: false,
+      controls: false,
+      repeat: false,
+      displayDescription: true,
+      displayTitle: true,
+    };
+  });
+
+  const onIdle = () => {
+    console.log('123');
+    setIsPlaying(false);
+    console.log(isPlaying);
+    setTimeout(() => {
+      console.log(selAnswer);
+      if (selAnswer === '') {
+        Alert.alert('Time out', 'Message', [
+          {
+            text: 'OK',
+            onPress: async () => {
+              setVisible(true);
+              questionId++;
+              if (data.question_ids[questionId]) {
+                setIsPlaying(true);
+                setImageUrl(
+                  BASE_IMAGE_URL +
+                    data.question_ids[questionId].url +
+                    '/poster.jpg',
+                );
+                setVideoUrl(
+                  BASE_VIDEO_URL + data.question_ids[questionId].url + '.m3u8',
+                );
+                setAnswers(data.question_ids[questionId].answers);
+              }
+            },
+          },
+        ]);
+      }
+    }, 3000);
+    //setQuestionId(nextId + 1);
+    ///console.log('After Idle ID => ', nextId);
+  };
+
+  const onBeforePlay = () => {};
+  const handleAnswer = (sel) => {
+    console.log(sel);
+  }
+  const YesButton = ({title, handleAnswer, correct}) => {
+    return (
+      <TouchableOpacity
+        style={{
+          backgroundColor: '#7ed957',
+          flex: 1,
+          margin: 10,
+          padding: 10,
+          justifyContent: 'center',
+          alignItems: 'center',
+          borderRadius: 8,
+        }} onPress={()=>setSelAnswer(correct && 'Yes')}>
+        <Text
+          style={{
+            color: '#ffffff',
+            fontSize: 17,
+            paddingLeft: 20,
+            paddingRight: 20,
+            fontWeight: '900',
+          }}>
+          {title}
+        </Text>
+      </TouchableOpacity>
+    );
+  };
+
+  const NoButton = ({title, handleAnswer, correct}) => {
+    return (
+      <TouchableOpacity
+        style={{
+          backgroundColor: '#ff5757',
+          flex: 1,
+          margin: 10,
+          padding: 10,
+          justifyContent: 'center',
+          alignItems: 'center',
+          borderRadius: 8,
+        }} onPress={()=> setSelAnswer(correct && 'No')}>
+        <Text
+          style={{
+            color: '#ffffff',
+            fontSize: 17,
+            paddingLeft: 20,
+            paddingRight: 20,
+            fontWeight: '900',
+          }}>
+          {title}
+        </Text>
+      </TouchableOpacity>
+    );
+  };
+  const handleClick= () => {
+
+  }
+  const Answer = ({answer}) => {
+    return (
+      <View style={{alignItems: 'center'}}>
+        <Text style={{color: '#ffffff', fontWeight: '400', fontSize: 20}}>
+          {answer.label}
+        </Text>
+        <View style={{flexDirection: 'row'}}>
+          <YesButton title={answer.answer1} handleAnswer={handleAnswer} correct={answer.answer1 === answer.correct}/>
+          <NoButton title={answer.answer2} handleAnswer={handleAnswer} correct={answer.answer2 === answer.correct}/>
+        </View>
+      </View>
+    );
+  };
+  const AnswerView = ({answers}) => {
+    return (
+      <View style={{marginRight: 50, flex: 1, alignItems: 'center'}}>
+        <View
+          style={{
+            flexDirection: 'row',
+            alignItems: 'center',
+            width: 400,
+            justifyContent: 'space-between',
+            paddingLeft: 20,
+          }}>
+          <View>
+            <Text style={{color: '#ffffff', fontSize: 20}}>
+              {questionId + '/' + data.question_ids.length}
+            </Text>
+            <Text style={{color: '#ffffff', fontSize: 17}}>Pitch Count</Text>
+          </View>
+
+          <TouchableOpacity
+            style={{
+              backgroundColor: '#1E73BE',
+              margin: 10,
+              padding: 10,
+              justifyContent: 'center',
+              alignItems: 'center',
+              borderRadius: 8,
+            }}
+            onPress={handlePressVideo}>
+            <Text
+              style={{
+                color: '#ffffff',
+                fontSize: 17,
+                paddingLeft: 20,
+                paddingRight: 20,
+                fontWeight: '900',
+              }}>
+              Next
+            </Text>
+          </TouchableOpacity>
+        </View>
+        {isPlaying === false && (
+          <>
+            {answers.map((item) => (
+              <Answer answer={item} />
+            ))}
+
+            <Text style={{color: '#ffffff', fontWeight: '700', fontSize: 17}}>
+              TIMER
+            </Text>
+            <CountdownCircle
+              seconds={3}
+              radius={30}
+              borderWidth={8}
+              color="#ff003f"
+              bgColor="#fff"
+              textStyle={{fontSize: 20}}
+              onTimeElapsed={() => console.log('Elapsed!')}
+            />
+          </>
+        )}
+      </View>
+    );
+  };
+
+  const playVideo = () => {
+    player.current.play();
+    setVisible(false);
+  };
+
+  return (
+    <View style={{flex: 1, flexDirection: 'row'}}>
+      <Overlay
+        isVisible={visible}
+        onBackdropPress={playVideo}
+        supportedOrientations={['landscape']}
+        overlayStyle={styles.overlay}
+      />
+      <View
+        style={{
+          flex: 1,
+          width: '50%',
+          height: '100%',
+          justifyContent: 'center',
+        }}>
+        <JWPlayer
+          ref={player}
+          style={styles.player}
+          playlistItem={playlistItem}
+          onBeforePlay={onBeforePlay}
+          
+          onIdle={onIdle}
+        />
+      </View>
+      <View style={{flex: 1, width: '50%', backgroundColor: '#333333'}}>
+        <AnswerView answers={answers} />
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  player: {
+    flex: 1,
+    width: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  overlay: {
+    backgroundColor: 'rgba(255,255,255,1)',
+
+    right: '25%',
+  },
+});
+
+export default QuizView;
