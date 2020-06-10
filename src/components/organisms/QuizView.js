817eeb4be16e2a08b937db49b539c516250c153d
Catalina <catalina@Catalinas-Mac.local>
2020-06-10 06:24:47 -0700

    Fix: QuizScreen Style fixed

--- src/components/organisms/QuizView.js
+++ src/components/organisms/QuizView.js
@@ -10,13 +10,14 @@ import {
 } from 'react-native';
 import JWPlayer from 'react-native-jw-media-player';
 import Video from 'react-native-video';
-import {Overlay} from 'react-native-elements';
+import {Overlay, Icon} from 'react-native-elements';
 
 const BASE_IMAGE_URL = 'https://content.jwplatform.com/v2/media/';
 const BASE_VIDEO_URL = 'https://content.jwplatform.com/manifests/';
 //let questionId = 0;
 const QuizView = ({data}) => {
   const [questionId, setQuestionId] = useState(0);
+  const [answerVisible, setAnswerVisible] = useState(true);
   console.disableYellowBox = true;
   const [imageUrl, setImageUrl] = useState(
     BASE_IMAGE_URL + data.question_ids[questionId].url + '/poster.jpg',
@@ -27,9 +28,13 @@ const QuizView = ({data}) => {
   const [answers, setAnswers] = useState(data.question_ids[questionId].answers);
   const [isPlaying, setIsPlaying] = useState('');
   const [selAnswer, setSelAnswer] = useState('');
+  const [thumbs, setThumbs] = useState('');
   const [isPaused, setIsPaused] = useState(true);
+  const [showAnswer, setShowAnswer] = useState(false);
+  const [answerId, setAnswerId] = useState(0);
   const player = useRef(null);
   const [visible, setVisible] = useState(true);
+  const [changeStyle, setChangeStyle] = useState(false);
   const handlePressVideo = () => {
     // console.log(player.current.play());
   };
@@ -66,26 +71,28 @@ const QuizView = ({data}) => {
   });
 
   handleTimeElapsed = () => {
-      if (selAnswer === '') {
-        Alert.alert('Time out', 'Message', [
-          {
-            text: 'OK',
-            onPress: async () => {
-              setVisible(true);
+    if (selAnswer === '') {
+      Alert.alert('Time out', 'Message', [
+        {
+          text: 'OK',
+          onPress: async () => {
+            setVisible(true);
 
-              if (data.question_ids[questionId]) {
-                setIsPlaying(true);
+            if (data.question_ids[questionId]) {
+              setIsPlaying(true);
 
-                setAnswers(data.question_ids[questionId].answers);
-              }
-            },
+              setAnswers(data.question_ids[questionId].answers);
+              setShowAnswer(false);
+            }
           },
-        ]);
-      }
-  }
+        },
+      ]);
+    }
+  };
   const onIdle = () => {
     setIsPlaying(false);
     setIsPaused(true);
+    setShowAnswer(true);
     if (data.question_ids.length > questionId) {
       setQuestionId(questionId + 1);
       setImageUrl(
@@ -93,58 +100,41 @@ const QuizView = ({data}) => {
       );
       setVideoUrl(BASE_VIDEO_URL + data.question_ids[questionId].url + '.m3u8');
     }
-  }
+  };
 
   const onBeforePlay = () => {};
-  const handleAnswer = (sel) => {
+  const handleAnswer = (sel, id) => {
     setSelAnswer(sel);
-    console.log("selectedanswer=>==========>",sel);
-    if (sel != '') {
-      Alert.alert('Correct', 'Message', [
-        {
-          text: 'OK',
-          onPress: async () => {
-            setVisible(true);
+    console.log('selectedanswer=>==========>', sel);
+    if (sel == 1) setThumbs('correct');
+    else setThumbs('incorrect');
 
-            if (data.question_ids[questionId]) {
-              setIsPlaying(true);
-
-              setAnswers(data.question_ids[questionId].answers);
-            }
-          },
-        },
-      ]);
-    } else {
-      Alert.alert('Wrong', 'Message', [
-        {
-          text: 'OK',
-          onPress: async () => {
-            setVisible(true);
-
-            if (data.question_ids[questionId]) {
-              setIsPlaying(true);
-
-              setAnswers(data.question_ids[questionId].answers);
-            }
-          },
-        },
-      ]);
-    }
-    
+    setChangeStyle(true);
+    setAnswerId(id);
+    console.log('selectedanswerIDDDDD=>==========>', answerId);
+    setTimeout(() => {
+      // setVisible(true);
+      // setShowAnswer(false);
+      // setThumbs('');
+      // if (data.question_ids[questionId]) {
+      //   setIsPlaying(true);
+      //   setAnswers(data.question_ids[questionId].answers);
+      // }
+      setAnswerId(0);
+      setChangeStyle(false);
+    }, 2000);
   };
-  const YesButton = ({title, handleAnswer, correct}) => {
+  const YesButton = ({title, handleAnswer, correct, id}) => {
     return (
       <TouchableOpacity
-        style={{
-          backgroundColor: '#7ed957',
-          flex: 1,
-          margin: 10,
-          padding: 10,
-          justifyContent: 'center',
-          alignItems: 'center',
-          borderRadius: 8,
-        }}
-        onPress={() => handleAnswer(correct && 'Yes')}>
+        style={
+          changeStyle
+            ? correct == 1
+              ? styles.correctButtonStyle
+              : styles.YesButtonStyle
+            : styles.YesButtonStyle
+        }
+        onPress={() => handleAnswer(correct, id)}>
         {/* onPress={() => Alert.alert(correct && 'Yes')}> */}
         <Text
           style={{
@@ -160,19 +150,20 @@ const QuizView = ({data}) => {
     );
   };
 
-  const NoButton = ({title, handleAnswer, correct}) => {
+  const NoButton = ({title, handleAnswer, correct, id}) => {
+    console.log('1111111111111111=================',id,answerId, id == answerId)
     return (
       <TouchableOpacity
-        style={{
-          backgroundColor: '#ff5757',
-          flex: 1,
-          margin: 10,
-          padding: 10,
-          justifyContent: 'center',
-          alignItems: 'center',
-          borderRadius: 8,
-        }}
-        onPress={() => handleAnswer(correct && 'No')}>
+        style={
+          changeStyle
+            ? correct == 1
+              ? styles.correctButtonStyle
+              : styles.NoButtonStyle
+            : id == answerId
+            ? styles.wrongButtonStyle
+            : styles.NoButtonStyle
+        }
+        onPress={() => handleAnswer(correct, id)}>
         <Text
           style={{
             color: '#ffffff',
@@ -195,14 +186,16 @@ const QuizView = ({data}) => {
         </Text>
         <View style={{flexDirection: 'row'}}>
           <YesButton
-            title={answer.answer1}
+            title={answer.answer1.answer}
             handleAnswer={handleAnswer}
-            correct={answer.answer1 === answer.correct}
+            correct={answer.answer1.correct}
+            id={answer.answer1.id}
           />
           <NoButton
-            title={answer.answer2}
+            title={answer.answer2.answer}
             handleAnswer={handleAnswer}
-            correct={answer.answer2 === answer.correct}
+            correct={answer.answer2.correct}
+            id={answer.answer2.id}
           />
         </View>
       </View>
@@ -221,7 +214,7 @@ const QuizView = ({data}) => {
           }}>
           <View>
             <Text style={{color: '#ffffff', fontSize: 20}}>
-              {(questionId + 1) + '/' + data.question_ids.length}
+              {questionId + 1 + '/' + data.question_ids.length}
             </Text>
             <Text style={{color: '#ffffff', fontSize: 17}}>Pitch Count</Text>
           </View>
@@ -248,25 +241,23 @@ const QuizView = ({data}) => {
             </Text>
           </TouchableOpacity>
         </View>
-        {isPlaying === false && (
-          <>
-            {answers.map((item) => (
-              <Answer answer={item} />
-            ))}
+        {answers.map((item) => (
+          <Answer answer={item} />
+        ))}
 
-            <Text style={{color: '#ffffff', fontWeight: '700', fontSize: 17}}>
-              TIMER
-            </Text>
-            <CountdownCircle
-              seconds={3}
-              radius={30}
-              borderWidth={8}
-              color="#ff003f"
-              bgColor="#fff"
-              textStyle={{fontSize: 20}}
-              onTimeElapsed={handleTimeElapsed}
-            />
-          </>
+        <Text style={{color: '#ffffff', fontWeight: '700', fontSize: 17}}>
+          TIMER
+        </Text>
+        {isPlaying === false && (
+          <CountdownCircle
+            seconds={3}
+            radius={30}
+            borderWidth={8}
+            color="#ff003f"
+            bgColor="#fff"
+            textStyle={{fontSize: 20}}
+            onTimeElapsed={handleTimeElapsed}
+          />
         )}
       </View>
     );
@@ -280,12 +271,6 @@ const QuizView = ({data}) => {
 
   return (
     <View style={{flex: 1, flexDirection: 'row'}}>
-      <Overlay
-        isVisible={visible}
-        onBackdropPress={playVideo}
-        supportedOrientations={['landscape']}
-        overlayStyle={styles.overlay}
-      />
       <View
         style={{
           flex: 1,
@@ -293,6 +278,108 @@ const QuizView = ({data}) => {
           height: '100%',
           justifyContent: 'center',
         }}>
+        {/* <Overlay
+          isVisible={visible}
+          onBackdropPress={playVideo}
+          supportedOrientations={['landscape']}
+          overlayStyle={styles.overlayVideo}
+          backdropStyle={styles.backdropVideo}
+          children={Video}
+        /> */}
+        {visible && (
+          <TouchableOpacity
+            onPress={playVideo}
+            style={{
+              position: 'absolute',
+              flex: 1,
+              alignItems: 'center',
+              justifyContent: 'center',
+              left: 0,
+              top: 0,
+              width: '100%',
+              height: '100%',
+              backgroundColor: 'rgba(33,33,33,0.5)',
+              zIndex: 300,
+            }}>
+            {/* {<i class="fa fa-play" aria-hidden="true"></i>} */}
+            <Icon
+              name="play"
+              type="font-awesome"
+              color="#ffffff"
+              size={100}></Icon>
+          </TouchableOpacity>
+        )}
+        {showAnswer == true && (
+          <TouchableOpacity
+            style={{
+              position: 'absolute',
+              flex: 1,
+              alignItems: 'center',
+              justifyContent: 'center',
+              left: 0,
+              top: 0,
+              width: '100%',
+              height: '100%',
+              backgroundColor: 'rgba(0,0,0,1)',
+              zIndex: 300,
+            }}>
+            {thumbs === 'incorrect' && (
+              <View
+                style={{
+                  alignItems: 'center',
+                  alignSelf: 'center',
+                  justifyContent: 'center',
+                }}>
+                <Icon
+                  name="thumbs-down"
+                  type="font-awesome"
+                  color="rgba(109,216,101,1)"
+                  size={200}></Icon>
+                <Text
+                  style={{
+                    position: 'absolute',
+                    alignItems: 'center',
+                    alignSelf: 'center',
+                    justifyContent: 'center',
+                    fontSize: 30,
+                    fontStyle: 'italic',
+                    fontWeight: '900',
+                    color: '#ffffff',
+                  }}>
+                  INCORRECT
+                </Text>
+              </View>
+            )}
+            {thumbs === 'correct' && (
+              <View
+                style={{
+                  alignItems: 'center',
+                  alignSelf: 'center',
+                  justifyContent: 'center',
+                }}>
+                <Icon
+                  name="thumbs-up"
+                  type="font-awesome"
+                  color="rgba(255,89,85,1)"
+                  size={200}></Icon>
+                <Text
+                  style={{
+                    paddingTop: 50,
+                    position: 'absolute',
+                    alignItems: 'center',
+                    alignSelf: 'center',
+                    justifyContent: 'center',
+                    fontSize: 30,
+                    fontStyle: 'italic',
+                    fontWeight: '900',
+                    color: '#ffffff',
+                  }}>
+                  CORRECT
+                </Text>
+              </View>
+            )}
+          </TouchableOpacity>
+        )}
         <Video
           ref={player}
           style={styles.player}
@@ -304,6 +391,18 @@ const QuizView = ({data}) => {
         />
       </View>
       <View style={{flex: 1, width: '50%', backgroundColor: '#333333'}}>
+        {(isPlaying == true || isPlaying === '') && (
+          <View
+            style={{
+              position: 'absolute',
+              left: 0,
+              top: 0,
+              width: '100%',
+              height: '100%',
+              backgroundColor: 'rgba(33,33,33,0.8)',
+              zIndex: 300,
+            }}></View>
+        )}
         <AnswerView answers={answers} />
       </View>
     </View>
@@ -316,11 +415,32 @@ const styles = StyleSheet.create({
     width: '100%',
     justifyContent: 'center',
     alignItems: 'center',
+    zIndex: 200,
   },
-  overlay: {
-    backgroundColor: 'rgba(255,255,255,1)',
 
-    right: '25%',
+  correctButtonStyle: {
+    backgroundColor: 'rgba(255, 166, 40, 0.8)',
+  },
+  wrongButtonStyle: {
+    backgroundColor: 'rgba(0,0,0,1)',
+  },
+  NoButtonStyle: {
+    backgroundColor: '#ff5757',
+    flex: 1,
+    margin: 10,
+    padding: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 8,
+  },
+  YesButtonStyle: {
+    backgroundColor: '#7ed957',
+    flex: 1,
+    margin: 10,
+    padding: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 8,
   },
 });
 
